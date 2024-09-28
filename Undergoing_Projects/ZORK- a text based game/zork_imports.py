0c87fe0f04c6c112510b7b107980f848eeda83c6
Mikael Kopperud <144001096+MikaelKOPP@users.noreply.github.com>
2024-09-29 01:10:40 +0200

    Create zork_imports.py

---
+++ Undergoing_Projects/ZORK- a text based game/zork_imports.py
@@ -0,0 +1,75 @@
+
+bossnames =['Zorg', 'Schlawg', 'Terraju', 'Hydra', 'Desmond', 'Viesrion']
+
+items = {'Healing Potion':       5, # heals player 50
+         'Increased DMG Potion': 4, # increases dmg mulitplier by 1.3x
+         'Luck Potion' :         3, # returns more coins for a few turns
+         'MAX Health Potion' :   2, # +20 max health
+         'Clairvoyance Potion' : 1, # reveals path before you before deciding
+         'Antidote' : 3 }
+
+opposite_directions = {'n': 's', 
+                       's': 'n', 
+                       'w': 'e', 
+                       'e': 'w' }
+
+upgradesToWeapons ={'Bleed': 12,  # 5 points of damage for each turn
+                    'Extra Sharp': 10,  # + 10 damage to any attack
+                    'Lethal Blade': 20, # 10% of INSTA KILL
+                    'Stun Adapter': 15} # 20% to paralyze the boss (skips its turn)
+
+biome_names = [
+    "Mystic Forest",
+    "Crimson Desert",
+    "Ethereal Plains",
+    "Frozen Tundra",
+    "Shadow Swamp",
+    "Golden Savannah",
+    "Emerald Jungle",
+    "Volcanic Wasteland",
+    "Crystal Caverns",
+    "Azure Lagoon",
+    "Thunderous Peaks",
+    "Luminous Marsh",
+    "Sunlit Grove",
+    "Icy Fjord",
+    "Forgotten Ruins",
+    "Blazing Sands",
+    "Gloomy Mire",
+    "Whispering Woods",
+    "Shimmering Reef",
+    "Twilight Thicket",
+    "Obsidian Badlands",
+    "Moonlit Prairie",
+    "Radiant Glade",
+    "Haunted Hollow",
+    "Celestial Meadows",
+    "Windswept Steppe",
+    "Enchanted Vale",
+    "Stormy Expanse",
+    "Verdant Expanse",
+    "Cursed Fen"
+]
+
+staff_names = [
+    "Staff of Eternal Flame",
+    "Shadowbinder's Rod",
+    "Arcane Whisper",
+    "Glacial Scepter",
+    "Stormbringer's Staff",
+    "Mystic Willow Staff",
+    "Staff of the Celestial Sphere",
+    "Voidweaver's Cane",
+    "Aetherflare Wand",
+    "Emberheart Staff",
+    "Nightfall Scepter",
+    "Staff of the Emerald Enclave",
+    "Voidshard Scepter",
+    "Starfire Rod",
+    "Moonlight Scepter",
+    "Staff of the Ancients",
+    "Soulbinder's Cane",
+    "Frostwyrm's Staff",
+    "Stormweaver's Rod",
+    "Enigma's Staff"
+]
